6fde3fed07b7d3706d0330d74676a7a5b1bd58b0
Mohammad Jangda <batmoo@gmail.com>
2013-11-16 18:56:28 +0000

    Cleanup sitemap invalidation
    * Use add_query_arg to generate URLs
    * Remove unnecessary function_exists

--- wpcom-helper.php
+++ wpcom-helper.php
@@ -15,15 +15,13 @@ function msm_sitemap_queue_nginx_cache_invalidation( $sitemap_id, $year, $month,
 	if ( ! function_exists( 'queue_async_job' ) )
 		return;
 
-	$site_url = home_url();
+	$sitemap_url = home_url( '/sitemap.xml' );
 
 	$sitemap_urls = array(
-		$site_url . "/sitemap.xml?yyyy=$year",
-		$site_url . "/sitemap.xml?yyyy=$year&mm=$month",
-		$site_url . "/sitemap.xml?yyyy=$year&mm=$month&dd=$day",
+		add_query_arg( array( 'yyyy' => $year ), $sitemap_url ),
+		add_query_arg( array( 'yyyy' => $year, 'mm' => $month ), $sitemap_url ),
+		add_query_arg( array( 'yyyy' => $year, 'mm' => $month, 'dd' => $day ), $sitemap_url ),
 	);
 
-	if ( function_exists( 'queue_async_job' ) ) {
-		queue_async_job( array( 'output_cache' => array( 'url' => $sitemap_urls ) ), 'wpcom_invalidate_output_cache_job', -16 );
-	}
+	queue_async_job( array( 'output_cache' => array( 'url' => $sitemap_urls ) ), 'wpcom_invalidate_output_cache_job', -16 );
 }
