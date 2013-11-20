cecccb9631a2ddcd363e841d3782bf31f9967dcf
Mohammad Jangda <batmoo@gmail.com>
2013-11-20 06:29:01 +0000

    wpcom-helper:
    * Offload update requests to async jobs.
    * Don't hook actions if async jobs don't exist.

--- wpcom-helper.php
+++ wpcom-helper.php
@@ -1,8 +1,19 @@
 <?php
 
-add_action( 'msm_insert_sitemap_post', 'msm_sitemap_queue_nginx_cache_invalidation', 10, 4 );
-add_action( 'msm_delete_sitemap_post', 'msm_sitemap_queue_nginx_cache_invalidation', 10, 4 );
-add_action( 'msm_update_sitemap_post', 'msm_sitemap_queue_nginx_cache_invalidation', 10, 4 );
+if ( function_exists( 'queue_async_job' ) ) {
+	add_action( 'msm_update_sitemap_for_year_month_date', array( __CLASS__, 'msm_wpcom_schedule_sitemap_update_for_year_month_date' ), 10, 2 );
+}
+
+function msm_wpcom_schedule_sitemap_update_for_year_month_date( $date, $time ) {
+	$data = (object) array( 'date' => $date );
+	queue_async_job( $data, 'vip_async_generate_sitemap' );
+}
+
+if ( function_exists( 'queue_async_job' ) ) {
+	add_action( 'msm_insert_sitemap_post', 'msm_sitemap_wpcom_queue_cache_invalidation', 10, 4 );
+	add_action( 'msm_delete_sitemap_post', 'msm_sitemap_wpcom_queue_cache_invalidation', 10, 4 );
+	add_action( 'msm_update_sitemap_post', 'msm_sitemap_wpcom_queue_cache_invalidation', 10, 4 );
+}
 
 /**
  * Queue action to invalidate nginx cache if on WPCOM
@@ -11,10 +22,7 @@ add_action( 'msm_update_sitemap_post', 'msm_sitemap_queue_nginx_cache_invalidati
  * @param string $month
  * @param string $day
  */
-function msm_sitemap_queue_nginx_cache_invalidation( $sitemap_id, $year, $month, $day ) {
-	if ( ! function_exists( 'queue_async_job' ) )
-		return;
-
+function msm_sitemap_wpcom_queue_cache_invalidation( $sitemap_id, $year, $month, $day ) {
 	$sitemap_url = home_url( '/sitemap.xml' );
 
 	$sitemap_urls = array(
