4a2ac11ff459b104cbd81c08a621886b945b3d23
Mobius5150 <mike@michaelblouin.ca>
2014-01-09 19:09:54 +0000

    Added UT coverage for sitemap statistics and fixed the existing UT.
    Added a base class MSM_SiteMap_Test that implements some functionality like creating dummy posts and faking cron that all test cases require. The test classes could not inherit this class as it caused strange behaviour with WordPress Tests, so test classes just create a private instance of the class.
    
    Added the test_sitemap_stats.php file to test statistics for post creation & deletion.
    
    Fixed the test_sitemap_creation.php tests. Previously they could potentially provide a badly formatted date string for the post_date of a new post (eg '2012-01-8' -- note the lack of trailing zeros). To fix this I used the DateTime() class to format the date inside MSM_SiteMap_Test.

--- tests/test_sitemap_creation.php
+++ tests/test_sitemap_creation.php
@@ -1,50 +1,44 @@
 <?php
 
+require_once( 'msm_sitemap_test.php');
+
 class WP_Test_Sitemap_Creation extends WP_UnitTestCase {
 	
 	public $core = null;
 	
+        private $num_days = 4;
 	private $posts = array();
 	private $posts_created = array();
+        private $test_base;
 
 	/**
 	 * Generate posts and build the sitemap
 	 */
 	function setup() {
-		// Add a post for that last few days
-                $num_days = 4;
-		for ( $i = 0; $i < $num_days; $i++ ) {
-			$post_data = array(
-				'post_title' => (string) uniqid(),
-                                'post_type' => 'post',
-				'post_content' => (string) uniqid(),
-				'post_status' => 'publish',
-                                'post_author' => 1,
-			);
-                        
-			$post_data['post_date'] = $post_data['post_modified'] = date( 'Y' ). '-' . date('m') . '-' . ( (int)date('d') - $i - 1 ) . ' 12:00:00';
-			$post_data['ID'] = wp_insert_post( $post_data, true );
-                        if ( is_wp_error($post_data['ID']) || 0 == $post_data['ID'] )
-                                throw new Exception ("Error: WP Error encountered inserting post. {$post_data['ID']->errors}, {$post_data['ID']->error_data}");
-                                
-			$this->posts_created[] = $post_data['ID'];
-			$this->posts[] = $post_data;
-		}
+                $this->test_base = new MSM_SiteMap_Test();
                 
-                $this->assertCount($num_days, $this->posts);
-		$this->build_sitemaps();
+                // Create posts for the last num_days days
+                $dates = array();
+		for ( $i = 0; $i < $this->num_days; $i++ )
+                    $dates[] = date( 'Y' ). '-' . date('m') . '-' . ( (int) date('d') - $i - 1 );
+                
+                $this->test_base->create_dummy_posts($dates);
+                
+                $this->assertCount($this->num_days, $this->test_base->posts);
+		$this->test_base->build_sitemaps();
 	}
 
 	/**
 	 * Remove the sample posts and the sitemap posts
 	 */
 	function teardown() {
+                $this->test_base->posts = array();
 		$sitemaps = get_posts( array(
 			'post_type' => Metro_Sitemap::SITEMAP_CPT,
 			'fields' => 'ids',
 			'posts_per_page' => -1,
 		) );
-		array_map( 'wp_delete_post', array_merge( $this->posts_created, $sitemaps ) );
+		array_map( 'wp_delete_post', array_merge( $this->test_base->posts_created, $sitemaps ) );
 	}
 
 	/**
@@ -59,11 +53,11 @@ class WP_Test_Sitemap_Creation extends WP_UnitTestCase {
 			'posts_per_page' => -1,
 		) );
                 
-		$this->assertCount( 4, $sitemaps );
+		$this->assertCount( $this->num_days, $sitemaps );
                 
 		foreach ( $sitemaps as $i => $map_id ) {
 			$xml = get_post_meta( $map_id, 'msm_sitemap_xml', true );
-			$post_id = $this->posts[$i]['ID'];
+			$post_id = $this->test_base->posts[$i]['ID'];
 			$this->assertContains( 'p=' . $post_id, $xml );
 
 			$xml_struct = simplexml_load_string( $xml );
@@ -79,40 +73,4 @@ class WP_Test_Sitemap_Creation extends WP_UnitTestCase {
 			wp_reset_postdata();
 		}
 	}
-
-	/**
-	 * Generate sitemaps; pretends to run cron six times
-	 */
-	private function build_sitemaps() {
-		_set_cron_array( array() ); // isolate our cron jobs.
-		MSM_Sitemap_Builder_Cron::reset_sitemap_data();
-		delete_option( 'msm_stop_processing' );
-		MSM_Sitemap_Builder_Cron::generate_full_sitemap();
-		update_option( 'msm_sitemap_create_in_progress', true );
-                
-		$this->fake_cron(); // this year
-		$this->fake_cron(); // this month
-		$this->fake_cron(); // today
-		$this->fake_cron(); // yesterday
-		$this->fake_cron(); // two days ago
-		$this->fake_cron(); // three days ago
-                $this->fake_cron();
-	}
-
-	/**
-	 * Fakes a cron job
-	 */
-	private function fake_cron() {
-		$schedule = _get_cron_array();
-		foreach ( $schedule as $timestamp => $cron ) {
-			foreach ( $cron as $hook => $arg_wrapper ) {
-				if ( substr( $hook, 0, 3 ) !== 'msm' ) continue; // only run our own jobs.
-				$arg_struct = array_pop( $arg_wrapper );
-				$args = $arg_struct['args'][0];
-				wp_unschedule_event( $timestamp, $hook, $arg_struct['args'] );
-				do_action( $hook, $args );
-			}
-		}
-	}
-
 }
