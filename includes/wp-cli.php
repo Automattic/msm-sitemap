17dc26e41a79583d1a72559a70912edff4de6443
Paul Kevan <paul.kevan@ukmetro.co.uk>
2014-01-31 17:17:17 +0000

    [issue-58] Adding cli to try and get more accurate amount of urls currently in the sitemaps

--- includes/wp-cli.php
+++ includes/wp-cli.php
@@ -181,6 +181,42 @@ class Metro_Sitemap_CLI extends WP_CLI_Command {
 		return true;
 	}
 
+
+	/**
+	 * @subcommand recount-indexed-posts
+	 */
+	public function recount_indexed_posts() {
+
+		$all_sitemaps = get_posts(
+			array(
+				'post_type' => Metro_Sitemap::SITEMAP_CPT,
+				'post_status' => 'publish',
+				'fields' => 'ids',
+				'suppress_filters' => false,
+				'posts_per_page' => -1,
+			)
+		);
+
+		$count_array = array();
+		$count = 0;
+
+		foreach ( $all_sitemaps as $sitemap_id ) {
+
+			$title = get_the_title( $sitemap_id );
+			$xml_data = get_post_meta( $sitemap_id, 'msm_sitemap_xml', true );
+
+			$xml = simplexml_load_string( $xml_data );
+			$count = count( $xml->url );
+			$count_array[$title] = $count;
+
+			$total_count += $count;
+		}
+
+		update_option( 'msm_sitemap_indexed_url_count', $count_array );
+		WP_CLI::line( sprintf( 'Total posts found: %s', $total_count ) );
+
+	}
+
 	/**
 	 * Check if the user has flagged to bail on sitemap generation.
 	 *
