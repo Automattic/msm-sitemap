7deaf6ae4b0d93f041b6dda081e9096a19f7edb1
Paul Kevan <pk@automattic.com>
2016-10-14 09:57:05 +0100

    Fix PHPCS warning

--- vipgo-helper.php
+++ vipgo-helper.php
@@ -1,7 +1,7 @@
 <?php
 
 // On VIP Go we're blocking using cron as it creates a mess of cron for large datasets
-add_filter('msm_sitemap_use_cron_builder', '__return_false', 9999);
+add_filter( 'msm_sitemap_use_cron_builder', '__return_false', 9999 );
 
 // Add the update cron, since this is necessary for ongoing updates
 add_action( 'msm_update_sitemap_for_year_month_date', 'vipgo_schedule_sitemap_update_for_year_month_date', 10, 2 );
