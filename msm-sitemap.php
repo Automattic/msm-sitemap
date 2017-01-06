90d94307a59eabc5d35251e7010a483c00be3a06
Matthew Denton <Matthew Denton>
2017-01-06 15:25:52 -0500

    update to return empty array instead of current year for no posts in db

--- msm-sitemap.php
+++ msm-sitemap.php
@@ -290,12 +290,14 @@ class Metro_Sitemap {
 
 		$oldest_post_date_gmt = $wpdb->get_var( "SELECT post_date FROM $wpdb->posts WHERE post_status = 'publish' ORDER BY post_date ASC LIMIT 1" );
 		
-		$oldest_post_year = $current_year = date( 'Y' );
 		if( null !== $oldest_post_date_gmt ) {
 			$oldest_post_year = date( 'Y', strtotime( $oldest_post_date_gmt ) );
+			$current_year = date( 'Y' );
+			return range( $oldest_post_year, $current_year );
+		} else {
+			return array();
 		}
 
-		return range( $oldest_post_year, $current_year );
 	}
 
 	/**
