e8edc3fb0b3a18d9a423156934891a3e7663defa
Matthew Denton <matt@mdbitz.com>
2017-01-09 09:33:32 -0500

    Cleanup of unused Post Generation in Unit Tests
    Original Tests were based on an integration logic and dependent on WP_Query returning an array of posts for testing against. After discussion with A8C team members we simplified the test to testing of the filter and post generation is no longer required.

--- tests/test-sitemap-filter.php
+++ tests/test-sitemap-filter.php
@@ -15,56 +15,6 @@ require_once( 'msm-sitemap-test.php' );
 class WP_Test_Sitemap_Filter extends WP_UnitTestCase {
 
 	/**
-	 * Humber of Posts to Create (1 per day)
-	 *
-	 * @var Integer
-	 */
-	private $num_days = 7;
-	
-	/**
-	 * Base Test Class Instance
-	 *
-	 * @var MSM_SIteMap_Test
-	 */
-	private $test_base;
-
-	/**
-	 * Generate posts and build initial sitemaps
-	 */
-	function setup() {
-		_delete_all_posts();
-
-		$this->test_base = new MSM_SiteMap_Test();
-
-		// Create posts for the last num_days days.
-		$dates = array();
-		$date = time();
-		for ( $i = 0; $i < $this->num_days; $i++ ) {
-			$date = strtotime( '-1 day', $date );
-			$dates[] = date( 'Y', $date ) . '-' . date( 'm', $date ) . '-' . date( 'd', $date );
-		}
-
-		$this->test_base->create_dummy_posts( $dates );
-
-		$this->assertCount( $this->num_days, $this->test_base->posts );
-		$this->test_base->build_sitemaps();
-	}
-
-	/**
-	 * Remove created posts, Sitemaps and options
-	 */
-	function teardown() {
-		$this->test_base->posts = array();
-		$sitemaps = get_posts( array(
-			'post_type' => Metro_Sitemap::SITEMAP_CPT,
-			'fields' => 'ids',
-			'posts_per_page' => -1,
-		) );
-		update_option( 'msm_sitemap_indexed_url_count' , 0 );
-		array_map( 'wp_delete_post', array_merge( $this->test_base->posts_created, $sitemaps ) );
-	}
-
-	/**
 	 * Verify that request for sitemap url doesn't cause Main Query to hit db.
 	 */
 	function test_bypass_main_query() {
