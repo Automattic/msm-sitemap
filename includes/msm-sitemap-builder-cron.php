dc381217610ea8677c93f460a4e1b92b1e7a11bd
Mohammad Jangda <batmoo@gmail.com>
2017-01-09 10:56:44 -0500

    Restore generate_sitemap_for_year_month_day
    We need something to actually trigger the scheduled cron events

--- includes/msm-sitemap-builder-cron.php
+++ includes/msm-sitemap-builder-cron.php
@@ -4,6 +4,7 @@ class MSM_Sitemap_Builder_Cron {
 
 	public static function setup() {
 		add_action( 'msm_update_sitemap_for_year_month_date', array( __CLASS__, 'schedule_sitemap_update_for_year_month_date' ), 10, 2 );
+		add_action( 'msm_cron_generate_sitemap_for_year_month_day', array( __CLASS__, 'generate_sitemap_for_year_month_day' ) );
 	}
 
 	public static function schedule_sitemap_update_for_year_month_date( $date, $time ) {
@@ -20,5 +21,22 @@ class MSM_Sitemap_Builder_Cron {
 					),
 				)
 			);
+	}
+
+	/**
+	 * Generate sitemap for a given year, month, day
+	 * @param mixed[] $args
+	 */
+	public static function generate_sitemap_for_year_month_day( $args ) {
+		$year = $args['year'];
+		$month = $args['month'];
+		$day = $args['day'];
+
+		$date_stamp = Metro_Sitemap::get_date_stamp( $year, $month, $day );
+		if ( Metro_Sitemap::date_range_has_posts( $date_stamp, $date_stamp ) ) {
+			Metro_Sitemap::generate_sitemap_for_date( $date_stamp );
+		} else {
+			Metro_Sitemap::delete_sitemap_for_date( $date_stamp );
+		}
 	}
 }
