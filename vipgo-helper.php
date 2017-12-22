d769b63c99c432a6ffa698240388b9b16f1211e8
Josh Betz <j@joshbetz.com>
2017-12-22 12:06:28 -0600

    Run on wp-cron on VIP Go
    VIP now offloads wp-cron to wp-cli
    
    https://vip.wordpress.com/2017/11/15/a-vip-cron/

--- vipgo-helper.php
+++ vipgo-helper.php
@@ -1,8 +1,5 @@
 <?php
 
-// On VIP Go we're blocking using cron as it creates a mess of cron for large datasets
-add_filter( 'msm_sitemap_use_cron_builder', '__return_false', 9999 );
-
 // Add the update cron, since this is necessary for ongoing updates
 add_action( 'msm_update_sitemap_for_year_month_date', 'vipgo_schedule_sitemap_update_for_year_month_date', 10, 2 );
 add_action( 'msm_vipgo_cron_generate_sitemap_for_year_month_day', 'vipgo_generate_sitemap_for_year_month_day' );
