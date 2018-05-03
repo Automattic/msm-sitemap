4d14a1f32f694cd5ab4768d670b60aa8d52d3b9a
Brandon Kraft <public@brandonkraft.com>
2018-05-03 15:53:39 +0000

    Enable PHP 5.2-5.4 compat

--- msm-sitemap.php
+++ msm-sitemap.php
@@ -575,11 +575,12 @@ class Metro_Sitemap {
 	 * Record deleted posts in an option to be used by get_last_modified_posts.
 	 *
 	 * This function saves one entry per date to avoid excessive db calls or option
-	 * size when deleting a large number of posts.
+	 * size when deleting a large number of posts on PHP 5.5+.
 	 */
 	public static function record_deleted_post( $post_id ) {
 		$deleted_posts = get_option( 'msm_sitemap_deleted_posts', array() );
 		$post          = get_post( $post_id );
+		$skip          = false;
 		if ( ! $post ) {
 			return;
 		}
@@ -588,8 +589,12 @@ class Metro_Sitemap {
 			// We want to standardize the post date to a specific date. Let's discard the time.
 			$date = date( 'Y-m-d', strtotime( $post->post_date ) );
 
-			// Let's check to see if the date is already on deck for regeneration.
-			if ( ! in_array( $date, array_column( $deleted_posts, 'post_date' ), true ) ) {
+			if ( function_exists( 'array_column' ) ) { // PHP 5.5+
+				// Let's check to see if the date is already on deck for regeneration.
+				$skip = in_array( $date, array_column( $deleted_posts, 'post_date' ), true );
+			}
+
+			if ( ! $skip ) {
 				$deleted_posts[] = array(
 					'ID'        => 0, // Array item is a placeholder to match modified post structure.
 					'post_date' => $date,
