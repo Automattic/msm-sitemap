ece63561cf0029605c2ef5a92e413fb71c3f04d5
Mohammad Jangda <mo@automattic.com>
2018-06-05 15:01:35 -0400

    Remove cron override code for VIP Go
    In #131, we restored the cron builder for VIP Go sites, but did not remove the related overrides, which means that we end up creating multiple events for each day which do the same thing.

--- vipgo-helper.php
+++ vipgo-helper.php
@@ -1,33 +1,3 @@
 <?php
 
-// Add the update cron, since this is necessary for ongoing updates
-add_action( 'msm_update_sitemap_for_year_month_date', 'vipgo_schedule_sitemap_update_for_year_month_date', 10, 2 );
-add_action( 'msm_vipgo_cron_generate_sitemap_for_year_month_day', 'vipgo_generate_sitemap_for_year_month_day' );
-
-function vipgo_schedule_sitemap_update_for_year_month_date( $date, $time ) {
-	list( $year, $month, $day ) = $date;
-
-	wp_schedule_single_event(
-		$time,
-		'msm_vipgo_cron_generate_sitemap_for_year_month_day',
-		array(
-			array(
-				'year' => (int) $year,
-				'month' => (int) $month,
-				'day' => (int) $day,
-			),
-		)
-	);
-}
-
-function vipgo_generate_sitemap_for_year_month_day( $args ) {
-	$year = $args['year'];
-	$month = $args['month'];
-	$day = $args['day'];
-	$date_stamp = Metro_Sitemap::get_date_stamp( $year, $month, $day );
-	if ( Metro_Sitemap::date_range_has_posts( $date_stamp, $date_stamp ) ) {
-		Metro_Sitemap::generate_sitemap_for_date( $date_stamp );
-	} else {
-		Metro_Sitemap::delete_sitemap_for_date( $date_stamp );
-	}
-}
+// VIP Go-specific code can go here...
