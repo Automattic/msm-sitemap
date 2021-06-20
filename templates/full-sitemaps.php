2c857593bce295e2ed587cd09cc439dad6247bc0
Kailey Lampert <trepmal@gmail.com>
2021-06-19 18:51:33 -0700

    phpcs cleanup for templates/full-sitemaps.php

--- templates/full-sitemaps.php
+++ templates/full-sitemaps.php
@@ -1,10 +1,18 @@
 <?php
+/**
+ * Sitemap template
+ *
+ * @package Metro_Sitemap
+ *
+ * phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+ * phpcs:disable WordPress.Security.NonceVerification.Recommended
+ */
 
 if ( ! Metro_Sitemap::is_blog_public() ) {
 	wp_die(
-		__( 'Sorry, this site is not public so sitemaps are not available.', 'msm-sitemap' ),
-		__( 'Sitemap Not Available', 'msm-sitemap' ),
-		array ( 'response' => 404 )
+		esc_html__( 'Sorry, this site is not public so sitemaps are not available.', 'msm-sitemap' ),
+		esc_html__( 'Sitemap Not Available', 'msm-sitemap' ),
+		array( 'response' => 404 )
 	);
 }
 
@@ -14,17 +22,22 @@ if ( empty( $req_year ) ) {
 }
 
 $req_month = ( isset( $_GET['mm'] ) ) ? intval( $_GET['mm'] ) : false;
-$req_day = ( isset( $_GET['dd'] ) ) ? intval( $_GET['dd'] ) : false;
+$req_day   = ( isset( $_GET['dd'] ) ) ? intval( $_GET['dd'] ) : false;
 
-$build_xml = Metro_Sitemap::build_xml( array( 'year' => $req_year, 'month' => $req_month, 'day' => $req_day ) );
+$build_xml = Metro_Sitemap::build_xml(
+	array(
+		'year'  => $req_year,
+		'month' => $req_month,
+		'day'   => $req_day,
+	)
+);
 
-if ( $build_xml === false ) {
+if ( false === $build_xml ) {
 	wp_die(
-		__( 'Sorry, no sitemap available here.', 'msm-sitemap' ),
-		__( 'Sitemap Not Available', 'msm-sitemap' ),
-		array ( 'response' => 404 )
+		esc_html__( 'Sorry, no sitemap available here.', 'msm-sitemap' ),
+		esc_html__( 'Sitemap Not Available', 'msm-sitemap' ),
+		array( 'response' => 404 )
 	);
 }
 header( 'Content-type: application/xml; charset=UTF-8' );
-echo $build_xml;
-?>
+echo $build_xml; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
