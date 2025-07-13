622d18f9a8c09e240a11e0558bfc8d91739d77c2
Gary Jones <gary.jones@automattic.com>
2025-07-13 22:13:05 +0100

    test: Add unit tests for sitemap index XML filters
    - Implemented tests for `msm_sitemap_index_appended_xml` and `msm_sitemap_index_xml` filters to verify correct XML appending and modification.
    - Ensured that the filters receive the expected arguments and return the correct XML structure, enhancing test coverage for sitemap customization features.

--- tests/FiltersTest.php
+++ tests/FiltersTest.php
@@ -75,5 +75,71 @@ class FiltersTest extends TestCase {
 
 		remove_all_filters( 'msm_sitemap_pre_get_post_year_range' );
 	}
+
+	/**
+	 * Verify that msm_sitemap_index_appended_xml filter can append XML and receives correct arguments.
+	 */
+	public function test_msm_sitemap_index_appended_xml_filter(): void
+	{
+		// Create a post and build sitemaps to ensure there is at least one sitemap.
+		$this->add_a_post_for_today();
+		$this->build_sitemaps();
+
+		$called_args = null;
+		$append_xml = '<sitemap><loc>https://example.com/custom.xml</loc></sitemap>';
+
+		add_filter(
+			'msm_sitemap_index_appended_xml',
+			function( $appended_xml, $year, $sitemaps ) use ( $append_xml, &$called_args ) {
+				$called_args = [ $appended_xml, $year, $sitemaps ];
+				return $append_xml;
+			},
+			10,
+			3
+		);
+
+		$xml = Metro_Sitemap::build_root_sitemap_xml();
+		$this->assertStringContainsString( $append_xml, $xml );
+		$this->assertIsArray( $called_args );
+		$this->assertEquals( '', $called_args[0] );
+		$this->assertFalse( $called_args[1] );
+		$this->assertIsArray( $called_args[2] );
+		$this->assertNotEmpty( $called_args[2] );
+
+		remove_all_filters( 'msm_sitemap_index_appended_xml' );
+	}
+
+	/**
+	 * Verify that msm_sitemap_index_xml filter can modify the final XML and receives correct arguments.
+	 */
+	public function test_msm_sitemap_index_xml_filter(): void
+	{
+		// Create a post and build sitemaps to ensure there is at least one sitemap.
+		$this->add_a_post_for_today();
+		$this->build_sitemaps();
+
+		$called_args = null;
+		$replacement_xml = '<?xml version="1.0"?><sitemapindex><sitemap><loc>https://example.com/override.xml</loc></sitemap></sitemapindex>';
+
+		add_filter(
+			'msm_sitemap_index_xml',
+			function( $xml_string, $year, $sitemaps ) use ( $replacement_xml, &$called_args ) {
+				$called_args = [ $xml_string, $year, $sitemaps ];
+				return $replacement_xml;
+			},
+			10,
+			3
+		);
+
+		$xml = Metro_Sitemap::build_root_sitemap_xml();
+		$this->assertEquals( $replacement_xml, $xml );
+		$this->assertIsArray( $called_args );
+		$this->assertStringContainsString( '<sitemapindex', $called_args[0] );
+		$this->assertFalse( $called_args[1] );
+		$this->assertIsArray( $called_args[2] );
+		$this->assertNotEmpty( $called_args[2] );
+
+		remove_all_filters( 'msm_sitemap_index_xml' );
+	}
 }
 
